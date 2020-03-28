d2badd2a6178a115da14b440eb410caae1b6f5c2
BlockSpaceVictor <54559440+BlockSpaceVictor@users.noreply.github.com>
2020-03-27 17:27:29 -0700

    added lives and game over

--- src/ball.js
+++ src/ball.js
@@ -1,12 +1,18 @@
+import { detectCollision } from "./collisionDetection";
+
 export default class Ball {
   constructor(game) {
-    this.speed = { x: 4, y: 4 };
-    this.position = { x: 10, y: 10 };
+    this.reset();
     this.size = 20;
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
     this.image = document.getElementById("img_ball");
     this.game = game;
+  }
+
+  reset() {
+    this.speed = { x: 4, y: -4 };
+    this.position = { x: 10, y: 400 };
   }
 
   draw(ctx) {
@@ -28,22 +34,17 @@ export default class Ball {
       this.speed.x = -this.speed.x;
     }
     //wall top or bottom
-    if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
+    if (this.position.y < 0) {
       this.speed.y = -this.speed.y;
     }
 
-    //check collision with paddle
-    let bottomOfBall = this.position.y + this.size;
-    let topOfPaddle = this.game.paddle.position.y;
-    let leftSideOfPaddle = this.game.paddle.position.x;
-    let rightSideOfPaddle =
-      this.game.paddle.position.x + this.game.paddle.width;
+    if (this.position.y + this.size > this.gameHeight) {
+      this.game.lives--;
+      this.reset();
+    }
 
-    if (
-      bottomOfBall >= topOfPaddle &&
-      this.position.x >= leftSideOfPaddle &&
-      this.position.x + this.size <= rightSideOfPaddle
-    ) {
+    //check collision with paddle
+    if (detectCollision(this, this.game.paddle)) {
       this.speed.y = -this.speed.y;
       this.position.y = this.game.paddle.position.y - this.size;
     }
--- src/game.js
+++ src/game.js
@@ -1,25 +1,102 @@
 import Paddle from "/src/paddle";
 import InputHandler from "/src/input";
 import Ball from "/src/ball";
+import Brick from "/src/brick";
+import { buildLevel, level1 } from "/src/levels";
+
+const GAMESTATE = {
+  PAUSED: 0,
+  RUNNING: 1,
+  MENU: 2,
+  GAMEOVER: 3
+};
 
 export default class Game {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
-  }
-  start() {
+    this.gamestate = GAMESTATE.MENU;
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
-    new InputHandler(this.paddle);
+    this.gameObjects = [];
+    new InputHandler(this.paddle, this);
+    this.lives = 1;
+  }
 
-    this.gameObjects = [this.ball, this.paddle];
+  start() {
+    if (this.gamestate !== GAMESTATE.MENU) return;
+    let bricks = buildLevel(this, level1);
+    this.gameObjects = [this.ball, this.paddle, ...bricks];
+    this.gamestate = GAMESTATE.RUNNING;
   }
 
   update(deltaTime) {
+    if (this.lives === 0) this.gamestate = GAMESTATE.GAMEOVER;
+
+    if (
+      this.gamestate === GAMESTATE.PAUSED ||
+      this.gamestate === GAMESTATE.MENU ||
+      this.gamestate === GAMESTATE.GAMEOVER
+    )
+      return;
+
     this.gameObjects.forEach(object => object.update(deltaTime));
+
+    this.gameObjects = this.gameObjects.filter(
+      object => !object.markedForDeletion
+    );
   }
 
   draw(ctx) {
     this.gameObjects.forEach(object => object.draw(ctx));
+
+    //paused game shade and text "Paused":
+    if (this.gamestate === GAMESTATE.PAUSED) {
+      ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.fillStyle = "rgba(0,0,0,0.5)";
+      ctx.fill();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("Paused", this.gameWidth / 2, this.gameHeight / 2);
+    }
+
+    //Menu draw
+    if (this.gamestate === GAMESTATE.MENU) {
+      ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.fillStyle = "rgba(0,0,0,1)";
+      ctx.fill();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText(
+        "Press Space To Start",
+        this.gameWidth / 2,
+        this.gameHeight / 2
+      );
+    }
+
+    //gameover draw
+    if (this.gamestate === GAMESTATE.GAMEOVER) {
+      ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.fillStyle = "rgba(0,0,0,1)";
+      ctx.fill();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("GAME OVER", this.gameWidth / 2, this.gameHeight / 2);
+    }
+  }
+
+  togglePause() {
+    // game states
+    if (this.gamestate === GAMESTATE.PAUSED) {
+      this.gamestate = GAMESTATE.RUNNING;
+    } else {
+      this.gamestate = GAMESTATE.PAUSED;
+    }
   }
 }
